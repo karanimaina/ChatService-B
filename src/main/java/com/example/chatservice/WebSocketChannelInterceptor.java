fa6837f43dc34c81276c3817a51d593ff588e64b
karanimaina <karani.maina2010@gmail.com>
2023-01-27 15:00:00 +0300

    add  a channel interceptor

---
+++ src/main/java/com/example/chatservice/WebSocketChannelInterceptor.java
@@ -0,0 +1,12 @@
+package com.example.chatservice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.stereotype.Service;
+
+import java.nio.channels.Channel;
+@Service
+@Slf4j
+public class WebSocketChannelInterceptor implements ChannelInterceptor {
+
+}
--- src/main/java/com/example/chatservice/config/WebSocketConfig.java
+++ src/main/java/com/example/chatservice/config/WebSocketConfig.java
@@ -1,4 +1,27 @@
 package com.example.chatservice.config;
 
-public class WebSocketConfig {
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+@Slf4j
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws-endpoint")
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+     registry.enableSimpleBroker("/queue","/topic");
+     registry.setApplicationDestinationPrefixes("/app");
+    }
 }
